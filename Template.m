31d10dab97d6d27b6e2f14b4af9cdf8b1228ef38
glesssard <lesg2605@usherbrooke.ca>
2019-08-26 18:31:27 -0400

    Added gitignore and example graph

--- Template.m
+++ Template.m
@@ -1,8 +1,18 @@
 %% Init
-
-int m=2
+clear
+clc
 
 %% Stuff 1
 
+m=2
+
 
 %% Stuff 2
+
+t=linspace(-100,100,1000);
+Y=1./(t.^2)+8;
+plot(t,Y)
+xlabel('Time (s)') 
+ylabel('Position')
+title('Test graph')
+legend({'Y = t^2+8'},'Location','southwest')
