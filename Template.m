1022cd391f7466ffe29e6c41e3d0320cd39c998b
glesssard <lesg2605@usherbrooke.ca>
2019-08-26 18:39:43 -0400

    Added stuff to example graph

--- Template.m
+++ Template.m
@@ -9,10 +9,17 @@ m=2
 
 %% Stuff 2
 
-t=linspace(-100,100,1000);
-Y=1./(t.^2)+8;
+t=linspace(-4,4, 1000);
+Y=t.^2 + 8;
+figure('Name','Test Graph Window','NumberTitle','off')
 plot(t,Y)
 xlabel('Time (s)') 
 ylabel('Position')
 title('Test graph')
 legend({'Y = t^2+8'},'Location','southwest')
+
+
+%% Clean
+
+clc
+clear
