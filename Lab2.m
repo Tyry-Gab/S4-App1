e04b365b36a06c3c4d836d6a60961690a2e69364
Thierry Constantin <dcelevol63@gmail.com>
2019-08-28 14:41:07 -0400

    Added blade's linear speed function plus its plot

--- Lab2.m
+++ Lab2.m
@@ -11,3 +11,9 @@ subplot(3,1,1)
 fplot('ThetaToBeta',[0 2*pi])
 xlabel('Theta (rad)')
 ylabel('Beta (rad)')
+
+% Blade's linear speed
+subplot(3,1,2)
+fplot('Blade_Lin_Speed',[0 2*pi] )
+xlabel('Theta (rad)')
+ylabel('VitA (m/s)')
